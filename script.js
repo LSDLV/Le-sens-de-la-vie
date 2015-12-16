83822be1d11065eea90e26677f68f26dd4cf6482
Sarah <saharah85@yahoo.fr>
2015-12-16 09:27:31 +0100

    on repart à zéro

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-// Create a rectangle shaped path with its top left point at
-// {x: 75, y: 75} and a size of {width: 75, height: 75}
-var path = new Path.Rectangle({
-	point: [75, 75],
-	size: [75, 75],
-	strokeColor: 'black'
-});
+// Créer un PaperScope Global
+paper.install(window);
 
-function onFrame(event) {
-	// Each frame, rotate the path by 3 degrees:
-	path.rotate(3);
-}
+// Exécuter le code une fois que le DOM est prêt
+window.onload = function() {
+
+	// Créer un projet vide et une view pour le canvas
+	paper.setup('myCanvas');
+
+	/* ============= Commencer à coder ici ==================*/
+	var path = new Path();
+	path.strokeColor = 'black';
+	var start = new Point(100, 100);
+	path.moveTo(start);
+	path.lineTo(start.add([ 200, -50 ]));
+	view.draw();
+
+
+	// Create a point whose x is between 0 and 50,
+	// and y is between 0 and 100
+	var point = new Point(50, 100).multiply(Point.random());
+	console.log(point);
+	// Create a size whose width is between 0 and 50,
+	// and height is between 0 and 100
+	var size = new Size(50, 100).multiply(Size.random());
+	console.log(size);
+	
+	/*=================== Fin du code ici ===================*/
+
+} // Fin de la fonction onload
