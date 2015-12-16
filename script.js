9cf09a0ee041cdc6a610f2605a954445226bdb20
ilikeslyz <adelben.mchindra@gmail.com>
2015-12-16 15:29:21 +0100

    ajout tunnel.css

--- script.js
+++ script.js
@@ -4,27 +4,30 @@ paper.install(window);
 // Exécuter le code une fois que le DOM est prêt
 window.onload = function() {
 
-	// Créer un projet vide et une view pour le canvas
-	paper.setup('myCanvas');
+    // Créer un projet vide et une view pour le canvas
+    paper.setup('myCanvas');
 
-	/* ============= Commencer à coder ici ==================*/
-	var path = new Path();
-	path.strokeColor = 'black';
-	var start = new Point(100, 100);
-	path.moveTo(start);
-	path.lineTo(start.add([ 200, -50 ]));
-	view.draw();
+    /* ============= Commencer à coder ici ==================*/
+    var path = new Path();
+    path.strokeColor = 'black';
+    var start = new Point(100, 100);
+    path.moveTo(start);
+    path.lineTo(start.add([200, -50]));
+    view.draw();
 
 
-	// Create a point whose x is between 0 and 50,
-	// and y is between 0 and 100
-	var point = new Point(50, 100).multiply(Point.random());
-	console.log(point);
-	// Create a size whose width is between 0 and 50,
-	// and height is between 0 and 100
-	var size = new Size(50, 100).multiply(Size.random());
-	console.log(size);
-	
-	/*=================== Fin du code ici ===================*/
+    // Create a point whose x is between 0 and 50,
+    // and y is between 0 and 100
+    var point = new Point(50, 100).multiply(Point.random());
+    console.log(point);
+    // Create a size whose width is between 0 and 50,
+    // and height is between 0 and 100
+    var size = new Size(50, 100).multiply(Size.random());
+    console.log(size);
 
-} // Fin de la fonction onload
+    var ctx = canvas.getContext("2d");
+    var tunnel = document.getElementByClass("all");
+    ctx.drawImage(tunnel, 0, 0);
+    /*=================== Fin du code ici ===================*/
+
+  } // Fin de la fonction onload
