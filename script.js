0d531dd1a1535d2e3a75d418f1c8b612818e0948
princessgirly <princessgirlyfactory@gmail.com>
2015-12-17 09:11:43 +0100

    nous sommes enfin sur la bonne voix

--- script.js
+++ script.js
@@ -8,23 +8,42 @@ window.onload = function() {
 	paper.setup('myCanvas');
 
 	/* ============= Commencer à coder ici ==================*/
-	var path = new Path();
-	path.strokeColor = 'black';
-	var start = new Point(100, 100);
-	path.moveTo(start);
-	path.lineTo(start.add([ 200, -50 ]));
-	view.draw();
+	/ Create a centered text item at the center of the view:
+	var text = new PointText({
+		point: view.center,
+		justification: 'center',
+		fontSize: 30,
+		fillColor: 'white'
+	});
 
+	// Define a random point in the view, which we will be moving
+	// the text item towards.
+	var destination = Point.random() * view.size;
 
-	// Create a point whose x is between 0 and 50,
-	// and y is between 0 and 100
-	var point = new Point(50, 100).multiply(Point.random());
-	console.log(point);
-	// Create a size whose width is between 0 and 50,
-	// and height is between 0 and 100
-	var size = new Size(50, 100).multiply(Size.random());
-	console.log(size);
-	
+	function onFrame(event) {
+		// Each frame, move the path 1/30th of the difference in position
+		// between it and the destination.
+
+		// The vector is the difference between the position of
+		// the text item and the destination point:
+		var vector = destination - text.position;
+
+		// We add 1/30th of the vector to the position property
+		// of the text item, to move it in the direction of the
+		// destination point:
+		text.position += vector / 30;
+
+		// Set the content of the text item to be the length of the vector.
+		// I.e. the distance it has to travel still:
+		text.content = Math.round(vector.length);
+
+		// If the distance between the path and the destination is less
+		// than 5, we define a new random point in the view to move the
+		// path to:
+		if (vector.length < 5) {
+			destination = Point.random() * view.size;
+		}
+	}
 	/*=================== Fin du code ici ===================*/
 
 } // Fin de la fonction onload
