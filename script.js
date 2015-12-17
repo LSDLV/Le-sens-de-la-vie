8c75b27f121ae394eee6fe635d4949a73ab76c61
Sarah <saharah85@yahoo.fr>
2015-12-17 13:08:28 +0100

    ajout du tetard statique

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.onload = function() {
     paper.setup('myCanvas');
 
 	/* ============= Commencer à coder ici ==================*/
-  	var path = new Path.Circle({
+  	var ovule = new Path.Circle({
   	center: view.center,
   	justification: 'center',
   	radius: 30,
@@ -18,16 +18,27 @@ window.onload = function() {
 	var destination = Point.random().multiply(view.size);
 
 	view.onFrame=function(event) {
-	    path.fillColor.hue += 1;
-		var vector = destination.subtract(path.position);
-		path.position=path.position.add(vector.divide(30)) ;
-		path.content = Math.round(vector.length);
+	    ovule.fillColor.hue += 1;
+		var vector = destination.subtract(ovule.position);
+		ovule.position=ovule.position.add(vector.divide(30)) ;
+		ovule.content = Math.round(vector.length);
 		if (vector.length < 75) {
 		destination = Point.random().multiply(view.size);
 		}
 	}
 
+	var headSperm = new Path.Circle(new Point(600, 300), 10);
+	headSperm.fillColor ='white';
+		
+	var tailSperm = new Path({
+		segments: [[600, 300], [680, 300]],
+		strokeWidth: 5,
+		strokeJoin:'round',
+		strokeColor:'white',
+		strokeCap: 'round'
+	});
+
 	/*=================== Fin du code ici ===================*/
 
-} // Fin de la fonction onload
+}; // Fin de la fonction onload
 
