b43ccc296f9826d8ef46721e00a2cec8d47e3cbb
ilikeslyz <adelben.mchindra@gmail.com>
2015-12-17 10:39:19 +0100

    recup modif

--- script.js
+++ script.js
@@ -62,29 +62,3 @@ view.onFrame = function(event) {
 
 	path.rotate(5);
 }
-
-    // Create a point whose x is between 0 and 50,
-    // and y is between 0 and 100
-    var point = new Point(50, 100).multiply(Point.random());
-    console.log(point);
-    // Create a size whose width is between 0 and 50,
-    // and height is between 0 and 100
-    var size = new Size(50, 100).multiply(Size.random());
-    console.log(size);
-
-    var ctx = canvas.getContext("2d");
-    var tunnel = document.getElementByClass("all");
-    ctx.drawImage(tunnel, 0, 0);
-
-
-
-
-
-
-
-
-
-    /*=================== Fin du code ici ===================*/
-
-  } // Fin de la fonction onload
->>>>>>> 9cf09a0ee041cdc6a610f2605a954445226bdb20
