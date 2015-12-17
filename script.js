b2376415ae1ee2713f7ef5ed5e0f36f557812a37
princessgirly <princessgirlyfactory@gmail.com>
2015-12-17 18:35:42 +0100

    rajout de commentaire explicatif

--- script.js
+++ script.js
@@ -8,38 +8,24 @@ window.onload = function() {
     paper.setup('myCanvas');
 
 	/* ============= Commencer à coder ici ==================*/
+  /*création du cercle avec un radius de 30*/
   var path = new Path.Circle({
   	center: view.center,
   	justification: 'center',
   	radius: 30,
   	fillColor: 'red'
 });
-var destination = Point.random().multiply(view.size);
+var destination = Point.random().multiply(view.size);/*définir une destination aléatoire du cercle dans la fenetre*/
 view.onFrame=function(event) {
-  path.fillColor.hue += 1;
-	var vector = destination.subtract(path.position);
-	path.position=path.position.add(vector.divide(30)) ;
-	path.content = Math.round(vector.length);
-	if (vector.length < 75) {
-		destination = Point.random().multiply(view.size);
+  path.fillColor.hue += 1; /*changement de la couleur du cercle en rajoutant 1 à la teinte */
+	var vector = destination.subtract(path.position);/*le vecteur=à la différence entre la position du cercle et le point de destination*/
+	path.position=path.position.add(vector.divide(30)) ;/*ajouter 1/30e du vecteur à la propriété position pour déplcer le cercle*/
+	path.content = Math.round(vector.length);/*définir le contenu du chemin : la distance qu'il doit parcourir*/
+	if (vector.length < 75) {     /*si la distance entre le chemin et la destination est inf à 75 alors destination=redéfini un point aléatoire*/
+		destination = Point.random().multiply(view.size);         /*dans la taille de la fenêtre*/
 	}
-  
+
 }
 }
 // view.draw();
 /*=================== Fin du code ici ===================
-// Create a circle shaped path at the center of the view,
-// with a radius of 70:
-var path = new Path.Circle({
-	center: view.center,
-	radius: 70,
-	fillColor: 'red'
-});
-
-function onFrame(event) {
-	// Each frame, change the fill color of the path slightly by
-	// adding 1 to its hue:
-	path.fillColor.hue += 1;
-}
-
-} // Fin de la fonction onload*/
